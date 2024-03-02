324b0bf2f5752dc4912142c93513d345187038ca
JISMON THOMAS <jismonthomas18@gmail.com>
2024-03-01 21:54:24 -0800

    added form tests

--- src/components/BookingForm.test.js
+++ src/components/BookingForm.test.js
@@ -1,47 +1,26 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import BookingForm from './BookingForm';
 
+const availableTimes = ['17:00', '17:30'];
+const submitData = jest.fn();
+
 test('Renders the BookingForm heading', () => {
     render(<BookingForm />);
     const headingElement = screen.getByText('Book Now');
     expect(headingElement).toBeInTheDocument();
 });
 
-// describe('BookingForm', () => {
-//     it('should render the form and handle form submission', () => {
-//         render(<BookingForm />);
+test(`disable sumbit button if fields are empty`, () => {
+    render(
+        <BookingForm availableTimes={availableTimes} updateTimes={submitData} />
+    );
 
-//         // Find form elements
-//         const firstNameInput = screen.getByLabelText(/first name/i);
-//         const lastNameInput = screen.getByLabelText(/last name/i);
-//         const emailInput = screen.getByLabelText(/email/i);
-//         const dateInput = screen.getByLabelText(/date/i);
-//         const timeSelect = screen.getByLabelText(/time/i);
-//         const guestsInput = screen.getByLabelText(/guests/i);
-//         const occasionSelect = screen.getByLabelText(/occasion/i);
-//         const submitButton = screen.getByText(/confirm reservation/i);
+    const emailInput = screen.getByLabelText(/email/i);
+    const submitButton = screen.getByText(/Confirm My Reservation/i);
 
-//         // Fill in form fields
-//         fireEvent.change(firstNameInput, { target: { value: 'John' } });
-//         fireEvent.change(lastNameInput, { target: { value: 'Doe' } });
-//         fireEvent.change(emailInput, {
-//             target: { value: 'johndoe@example.com' },
-//         });
-//         fireEvent.change(dateInput, { target: { value: '2023-06-01' } });
-//         fireEvent.change(timeSelect, { target: { value: '17:00' } });
-//         fireEvent.change(guestsInput, { target: { value: '2' } });
-//         fireEvent.change(occasionSelect, { target: { value: 'birthday' } });
+    fireEvent.change(emailInput, {
+        target: { value: 'johndoe@example.com' },
+    });
 
-//         // Expected form field values
-//         expect(firstNameInput.value).toBe('John');
-//         expect(lastNameInput.value).toBe('Doe');
-//         expect(emailInput.value).toBe('johndoe@example.com');
-//         expect(dateInput.value).toBe('2023-06-01');
-//         expect(timeSelect.value).toBe('17:00');
-//         expect(guestsInput.value).toBe('2');
-//         expect(occasionSelect.value).toBe('birthday');
-
-//         // Submit the form
-//         fireEvent.click(submitButton);
-//     });
-// });
+    expect(submitButton).toBeDisabled();
+});
