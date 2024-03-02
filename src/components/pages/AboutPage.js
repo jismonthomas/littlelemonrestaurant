1c01572d28a9123ec4a103514ed09759f8ff6ca3
JISMON THOMAS <jismonthomas18@gmail.com>
2024-03-01 20:44:42 -0800

    added about & menu pages

---
+++ src/components/pages/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AboutUs from '../AboutUs';
+
+const AboutPage = () => {
+    return (
+        <div className="AboutPage">
+            <AboutUs />
+        </div>
+    );
+};
+
+export default AboutPage;
---
+++ src/components/pages/MenuPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WeeklySpecials from '../WeeklySpecials';
+
+const MenuPage = () => {
+    return (
+        <div className="menuPage">
+            <WeeklySpecials />
+        </div>
+    );
+};
+
+export default MenuPage;
